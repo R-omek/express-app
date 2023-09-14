14083b80979d4c9bba9a66cb48490e01ec8237ee
roman hrytsiuk <rmngrck97@gmail.com>
2023-09-14 19:40:41 +0300

    added cart

--- public/app.js
+++ public/app.js
@@ -4,3 +4,40 @@ document.querySelectorAll('.price').forEach(node => {
         style: 'currency'
     }).format(node.textContent)
 })
+
+const $cart = document.querySelector('#cart')
+
+if ($cart) {
+    $cart.addEventListener('click', (event) => {
+       if (event.target.classList.contains('js-remove')) {
+        const id = event.target.dataset.id
+        
+        fetch('cart/remove/' + id, {
+            method: 'delete'
+        }).then(res => res.json())
+        .then(cart => {
+            if (cart.courses.length) {
+                const html = cart.courses.map(c => {
+                    return `
+                    <tr>
+                        <td>${c.title}</td>
+                        <td>${c.price}$</td>
+                        <td>${c.count}</td>
+                        <td>
+                            <button class="btn btn-small js-remove" data-id="${c.id}">Delete</button>
+                        </td>
+                    </tr>
+                    `
+                }).join('')
+                $cart.querySelector('tbody').innerHTML = html
+                $cart.querySelector('.price').textContent = new Intl.NumberFormat('en-En', {
+                    currency: 'usd',
+                    style: 'currency'
+                }).format(cart.price)
+            } else {
+                $cart.innerHTML = '<p>Cart is empty</p>'
+            }
+        })
+       }
+    })
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,28 @@
 const {Router} = require('express')
 const router = Router()
 const Cart = require('../models/cart')
+const Course = require('../models/course')
 
 router.post('/add', async (req, resp) => {
+    const course = await Course.getById(req.body.id)
+    await Cart.add(course)
+    resp.redirect('/cart')
+})
 
+router.get('/', async (req, resp) => {
+    const cart = await Cart.fetch()
+    resp.render('cart', {
+        title: 'Cart',
+        isCart: true,
+        courses: cart.courses,
+        price: cart.price
+    })
+})
+
+router.delete('/remove/:id', async (req, resp) => {
+    const cart = await Cart.delete(req.params.id)
+
+    resp.status(200).json(cart)
 })
 
 module.exports = router
